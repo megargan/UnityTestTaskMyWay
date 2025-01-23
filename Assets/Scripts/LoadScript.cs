8072bcae54ab703fdb5359640aa4bf7bfc7b0053
megargan <64369127+megargan@users.noreply.github.com>
2025-01-23 17:19:38 +0300

    Loading json files feature

--- Assets/Scripts/LoadScript.cs
+++ Assets/Scripts/LoadScript.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,10 @@ public class LoadScript : MonoBehaviour
     [SerializeField] private string bundlePath; // путь к бандлу который нужно загрузить
     [SerializeField] private string spriteName; // название файла со спрайтом для кнопки
     [SerializeField] private Button[] targetButtons; // кнопки к которым будет применен спрайт после загрузки 
+    [SerializeField] private CounterScript counterobject; // обьект с скриптом счетчика на котором будут обновляться данные
+    [SerializeField] private string settingsPath; //путь с json файлу с сохранением
+    [SerializeField] private string messageSettingsPath; //путь с json файлу с текстом сообщения
+    [SerializeField] private Text welcomeTextObject; // обьект на который будет загружен текст из файла
     
     
     public void UpdateButtonAction() // метод вызываемый при нажатии кнопки "обновить контент"
@@ -18,8 +23,9 @@ public class LoadScript : MonoBehaviour
     private Sprite _sprite;
     IEnumerator LoadData()
     {
-        yield return new WaitForSeconds(0.5f);
-      
+        yield return new WaitForSeconds(0.5f); // задержка для перехода в экран загрузки
+        LoadJsonCounter();
+        LoadJsonMessage();
         AssetBundle.UnloadAllAssetBundles(true); // выгрузить все ранее загруженные бандлы чтобы была возможность загрузить их снова
         
         Debug.Log("Starting load");
@@ -48,4 +54,49 @@ public class LoadScript : MonoBehaviour
        
         yield break;
     }
+    
+    
+    void LoadJsonCounter() // подгузка счета из json файла
+    {
+        if (File.Exists(settingsPath))
+        {
+            string json = File.ReadAllText(settingsPath);
+            CounterScript.Settings settings = JsonUtility.FromJson<CounterScript.Settings>(json);
+            counterobject.UpdateDataFromClass(settings);
+        }
+        else // файл создается если его не существует
+        {
+            CounterScript.Settings settings = new CounterScript.Settings
+            {
+                startingNumber = 0
+            };
+            string newjson = JsonUtility.ToJson(settings);
+            File.WriteAllText(settingsPath, newjson);
+        }
+    }
+
+    void LoadJsonMessage()
+    {
+        if (File.Exists(messageSettingsPath))
+        {
+            string json = File.ReadAllText(messageSettingsPath);
+            MessageJson messageJson = JsonUtility.FromJson<MessageJson>(json);
+            welcomeTextObject.text = messageJson.welcometext;
+        }
+        else
+        {
+            MessageJson message = new MessageJson
+            {
+                welcometext = "Добро пожаловать в счетчик!"
+            };
+            string newjson = JsonUtility.ToJson(message);
+            File.WriteAllText(messageSettingsPath, newjson);
+        }
+    }
+
+    class MessageJson
+    {
+        public string welcometext;
+    }
+    
 }
